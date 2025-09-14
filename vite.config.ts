72c486b8e581a80ef26f62cdfc739318a4b498ef
Mixer-sir <rmT13089@auca.kg>
2025-09-15 00:00:57 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,7 @@ import { defineConfig, Plugin } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { createServer } from "./server";
-import { defineConfig } from 'vite'
 
-
-export default defineConfig({
-  base: '/PortFolioo/', // название репозитория
-})
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
